1670d807bd23b03e27d774fa01871e10f2a0d7da
yuzarsif3465 <118628889+yuzarsif3465@users.noreply.github.com>
2024-03-09 16:04:37 +0300

    Add files via upload

---
+++ This/Ogrenci.java
@@ -0,0 +1,14 @@
+package This;
+
+public class Ogrenci {
+    private String name;
+    private String surname;
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+
+}
---
+++ This/Test.java
@@ -0,0 +1,11 @@
+package This;
+
+public class Test {
+    public static void main(String[] args) {
+        Ogrenci ogrenci1= new Ogrenci();
+        ogrenci1.setName("yusuf");
+        System.out.println("adı: "+ ogrenci1.getName());
+        ogrenci1.setName("ali");
+        System.out.println("adı: "+ ogrenci1.getName());
+    }
+}
